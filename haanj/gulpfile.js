caf2a254144ac7f792dbe10a86d6d734aef95887
haanj <joshua.haan@ymail.com>
2016-03-02 10:44:36 -0800

    replaces .eslintrc.json

--- haanj/gulpfile.js
+++ haanj/gulpfile.js
@@ -9,45 +9,7 @@ var tests = ['test/*js'];
 
 gulp.task('lint', function() {
   return gulp.src(paths)
-    .pipe(lint({
-      'rules': {
-        'no-console': 0,
-        'indent': [
-          2,
-          2
-        ],
-        'quotes': [
-          2,
-          'single'
-        ],
-        'linebreak-style': [
-          2,
-          'unix'
-        ],
-            'semi': [
-              2,
-              'always'
-            ]
-      },
-      'env': {
-        'es6': true,
-        'node': true,
-        'browser': true
-      },
-      'globals': {
-        'describe': false,
-        'it': false,
-        'beforeEach': false,
-        'afterEach': false,
-        'before': false,
-        'after': false
-      },
-      'ecmaFeatures': {
-        'modules': true,
-        'experimentalObjectRestSpread': true
-      },
-      'extends': 'eslint:recommended'
-    }))
+    .pipe(lint())
     .pipe(lint.format());
 });
 
