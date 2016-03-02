7a20f75c906da7b90b0becca6e8ce57f531c2de4
haanj <joshua.haan@ymail.com>
2016-03-02 10:20:44 -0800

    adds linter code into the gulpfile for some reason

--- haanj/gulpfile.js
+++ haanj/gulpfile.js
@@ -9,7 +9,45 @@ var tests = ['test/*js'];
 
 gulp.task('lint', function() {
   return gulp.src(paths)
-    .pipe(lint())
+    .pipe(lint({
+      'rules': {
+        'no-console': 0,
+        'indent': [
+          2,
+          2
+        ],
+        'quotes': [
+          2,
+          'single'
+        ],
+        'linebreak-style': [
+          2,
+          'unix'
+        ],
+            'semi': [
+              2,
+              'always'
+            ]
+      },
+      'env': {
+        'es6': true,
+        'node': true,
+        'browser': true
+      },
+      'globals': {
+        'describe': false,
+        'it': false,
+        'beforeEach': false,
+        'afterEach': false,
+        'before': false,
+        'after': false
+      },
+      'ecmaFeatures': {
+        'modules': true,
+        'experimentalObjectRestSpread': true
+      },
+      'extends': 'eslint:recommended'
+    }))
     .pipe(lint.format());
 });
 
@@ -18,15 +56,8 @@ gulp.task('mocha', function() {
     .pipe(mocha({reporter: 'nyan'}));
 });
 
-// gulp.task('mocha', function() {
-//   gulp.src(paths)
-//     .pipe(mocha());
-// });
-
 gulp.task('default', ['lint', 'mocha']);
 
 gulp.watch(paths, function(){
   gulp.run('default');
 });
-
-// stuff
